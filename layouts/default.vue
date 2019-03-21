0a2cfa702aa68d65e4d6cee4092d2e399165ebf7
ah100101 <ah100101@gmail.com>
2019-03-20 21:21:13 -0500

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <section class="main-content columns">
+
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/post.vue
@@ -0,0 +1,72 @@
+<style lang='scss'>
+  .post {
+    
+    .form {
+      padding: 2rem;
+    }
+  }
+</style>
+
+<template>
+  <section class="section post">
+    <h2>Post a Gift Idea</h2>
+    <div class="form">
+      <b-field horizontal label="Link to Gift"
+            v-bind:type="{ 'is-success' : validUrl }"
+            v-bind:message="validUrlMessage">
+            <b-input value="johnsilver"></b-input>
+      </b-field>
+      <b-field horizontal label='Price'>
+        <b-field>
+          <p class="control">
+              <span class="button is-static">$</span>
+          </p>
+          <b-input 
+            type="number" 
+            placeholder="0.00"
+            v-bind:value="price" 
+            expanded></b-input>
+        </b-field>
+      </b-field>
+      <b-field horizontal label="Message">    
+        <b-input 
+        type="textarea"
+        v-bind:value="message"></b-input>
+      </b-field>
+      <b-field horizontal>
+            <p class="control">
+                <button class="button is-primary">
+                  Post Gift
+                </button>
+            </p>
+        </b-field>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  components: {
+    
+  },
+  data () {
+    return {
+      message: '',
+      price: ''
+    }
+  },
+  computed: {
+    validUrl: function () {
+      // TODO: verify url with regex
+      return true;
+    },
+    validUrlMessage: function () {
+      // TODO: handle invalid url
+      if (this.validUrl) {
+        return 'Valid Gift Url'
+      }
+    }
+  }
+}
+</script>
